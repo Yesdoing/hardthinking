af71e1f418cf1a41613dd4341c389099b5864def
Jane Lee <Yesdoing13@gmail.com>
2017-09-22 23:45:48 +0900

    initial commit

---
+++ src/main/java/yesdoing/HardthinkingApplication.java
@@ -0,0 +1,12 @@
+package yesdoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardthinkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardthinkingApplication.class, args);
+	}
+}
