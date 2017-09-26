8807105bf7f29a8ba68a35324ecf1d10afcb0953
Jane Lee <Yesdoing13@gmail.com>
2017-09-26 20:23:13 +0900

    User 컨트롤러 생성, 회원가입 기능 구현

---
+++ src/main/java/yesdoing/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package yesdoing.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+}
--- src/main/java/yesdoing/web/UserController.java
+++ src/main/java/yesdoing/web/UserController.java
@@ -1,10 +1,20 @@
 package yesdoing.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import yesdoing.domain.User;
+import yesdoing.domain.UserRepository;
 
 @Controller
+@RequestMapping("/user/")
 public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	@GetMapping("/login")
 	public String login() {
@@ -14,5 +24,12 @@ public class UserController {
 	@GetMapping("/join")
 	public String join() {
 		return "user/signInForm";
+	}
+	
+	@PostMapping("/join")
+	public String joinUser(User user) {
+		System.out.println(user);
+		userRepository.save(user);
+		return "index";
 	}
 }
